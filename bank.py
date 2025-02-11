9be28d322103a97ffd0b8cda82bd73b1c32a34a1
Faolán Hamilton <faolanash01@gmail.com>
2025-02-11 23:25:02 +0000

    first attempt, steps 1 and 2 done just need to format

---
+++ bank.py
@@ -0,0 +1,13 @@
+#bank.py
+#Prompt the user and read in two money amounts (in cent),Add the two amounts and 
+#Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
+#Author: Faolán Hamilton
+
+#Prompt the user and read in two money amounts (in cent)
+Amount1 = int(input ("Amount1(in cent)"))
+Amount2 = int(input ("Amount2(in cent)"))
+#Add the two amounts 
+Sum = Amount1 + Amount2
+print (Sum)
+#Print out the answer in a human readable format with a euro sign and 
+#decimal point between the euro and cent of the amount 
