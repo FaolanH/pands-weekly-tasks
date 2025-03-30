574c7ca1c3ef35f3e8c9c3335017c7447e896565
Faolán Hamilton <faolanash01@gmail.com>
2025-03-30 21:09:14 +0100

    had to acknowledge Friday!

--- week5_weekday.py
+++ week5_weekday.py
@@ -14,9 +14,14 @@ today = datetime.datetime.now()
 #This step formats today's date as a weekday day (aka a number between 0-6 incl. 6)
 day_of_week = today.weekday()
 
-#Which day of the week is the date? The days start from 0 (Monday) and go up to 4 (Friday)
-if day_of_week <5:
+#Which day of the week is the date? The days start from 0 (Monday) and go up to 3 (Thursday)
+if day_of_week <4:
     print (f"Yes, unfortunately {today.strftime('%A, %B %d')} is a weekday")
+
+#Well if it's a Friday, I think there should be a bit of celebration!
+elif day_of_week == 5:
+    print (f"Yay, finally it's {today.strftime('%A, %B %d')}, still a weekday but nearly the weekend!")
+    
 #for a weekend, it is a 5 or a 6, but this can be called else as there is no other option
 else:
     print (f"Today ({today.strftime('%A, %B %d')}) is the weekend, yay!")
