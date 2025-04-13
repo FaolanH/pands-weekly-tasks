e53f30568328f17dc63879ecb8b270e2ceac1d59
Faolán Hamilton <faolanash01@gmail.com>
2025-04-13 16:58:34 +0100

    first try at week 7, I have read in the file, now need to add arguements

---
+++ week7_txtfile_e.py
@@ -0,0 +1,11 @@
+# week7_txtfile_e.py
+# Author: Faolán Hamilton
+
+# Task - read in a txt file and count the number of es
+
+#I learned this method with Datacamp: https://campus.datacamp.com/courses/introduction-to-importing-data-in-python/introduction-and-flat-files-1?ex=2
+#The file is read in, and prints
+with open ('loremipsum.txt', mode = 'r') as f:
+    print (f.read())
+
+
