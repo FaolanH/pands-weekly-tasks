5f1061a6a62d5aa0f29131487ce346a4e974798b
Faolán Hamilton <faolanash01@gmail.com>
2025-04-13 21:51:14 +0100

    adding a bit more, have figured some of the find letter 'e' out but needs more work!

--- week7_txtfile_e.py
+++ week7_txtfile_e.py
@@ -5,7 +5,34 @@
 
 #I learned this method with Datacamp: https://campus.datacamp.com/courses/introduction-to-importing-data-in-python/introduction-and-flat-files-1?ex=2
 #The file is read in, and prints
-with open ('loremipsum.txt', mode = 'r') as f:
+'''
+def lorem(*e):
+    for l in lorem:
+        print
+    with open ('loremipsum.txt', mode = 'r') as f:
     print (f.read())
+
+
+#loremfile = open("loremipsum.txt")
+def loremfile ():
+    with open ('loremipsum.txt', mode = 'r') as f:
+        print (f.read())
+
+print (loremfile)#.find("e"))  
+    
+#print (loremfile[0].find("e"))
+'''
+def loremfile(*e):
+    loremfile = open("loremipsum.txt","rt") as file:
+    f.read()
+
+    for letter in e:
+        loremfile += loremfile.find("e")
+    loremfile.close()
+    return loremfile
+
+print (loremfile)
+
+
 
 
