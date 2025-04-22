df94f600bf286b56fdd78a3da0441d968d51c059
Faolán Hamilton <faolanash01@gmail.com>
2025-04-22 21:46:00 +0100

    finished this task, added the correct structure

--- week4_collatz.py
+++ week4_collatz.py
@@ -1,22 +1,37 @@
-#week4_collatz.py
+# week4_collatz.py
+# Week 4 Assignment: User input an int, and depending on if it's odd or even have the program output collatz, ending in 1
+# Author: Faolán Hamilton 
 
-# ask user to input a positive integer and output successive value calculations 
-#At each step calculate the next value by taking the current value and, if it is even, 
-#divide it by two, but if it is odd, multiply it by three and add one.
-#Have the program end if the current value is one.
+# Initially, all I had was a while loop for even and odd numbers, as well as the user input. 
+# A stackoverview forum (listed as 'A' in references below) built on this method to output the desired collatz 
+# I tweaked it in a way that made sense to me, using the initial post to be refined and the last answer 
+# not including the 'seen', 'set()', 'try' and 'ValueError' and using only what I understood
 
-#Author: Faolán Hamilton
+#defining the collatz_input function
+def collatz_input(num):
+    while num != 1: # ensuring that if it is equal to 1, the loop will end
+        if num % 2: # any even number will be divisible by two 
+            num = 3 * num + 1 # any odd number if multiplied by three + 1 will be an even number, which can then be divided by 2 resulting in an int
+        else:
+            num //= 2 # divides the user input by 2
+        print(num, end=" ") # end=" " creates a better looking output, with all numbers on the same line with a space between them
+    return num # the forum had suggested a return == 1, but this seems redundant considering the initial exclusion
 
-user_input = int(input("Please enter a positive integer (whole number): "))
+# Now that the function has been defined, the number can be decided by the user!
+# Using int at least confirms the type of input
+num = int(input("Please enter a positive integer (whole number) greater than 1: "))
 
-#even_number = ({user_input} % 2)
-#odd_number = ({user_input} *3 % 1)
+#adding this if statement in lessens the chance for user error 
+if num <= 0:
+    print("Try again - please only enter a positive integer (whole number): ")
+# this is the final piece - now that everything is added/correct the function can be printed under the right conditions
+else:
+    collatz_input(num) 
 
-while (user_input % 2) == 0:
-    even = (user_input % 2)
-    print (even)
+# References
 
+# A: Collatz structure - https://stackoverflow.com/questions/75745136/collatz-sequence-in-python-any-improvements
+# B: formatting output using end=" " - https://stackoverflow.com/questions/18424899/print-range-of-numbers-on-same-line
 
-    
-if (user_input % 2) != 0:
-    print ("odd number")
+#----------End----------#
+
