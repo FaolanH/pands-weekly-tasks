5efc3889a5e73a188f7e0141f9f211089e6e291e
FaolanH <faolanash01@gmail.com>
2025-04-26 17:41:15 +0100

    adding more structure to week 1 task

--- week1_olleh.py
+++ week1_olleh.py
@@ -9,6 +9,6 @@ print("!dlroW ,olleH"[::-1])
 
 # References
 
-# Lecture PANDS 1.2 Running first python (Andrew Beatty) - https://atlantictu-my.sharepoint.com/personal/andrew_beatty_atu_ie/_layouts/15/stream.aspx?id=%2Fpersonal%2Fandrew%5Fbeatty%5Fatu%5Fie%2FDocuments%2Farchive%2Fpands%20private%20%282024%29%2Fslides%2FPANDS%201%2E2%20Running%20first%20python%2Emp4&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E07962988%2D253c%2D4376%2D95ae%2Df00070016457
+# A - Lecture PANDS 1.2 Running first python (Andrew Beatty) - https://atlantictu-my.sharepoint.com/personal/andrew_beatty_atu_ie/_layouts/15/stream.aspx?id=%2Fpersonal%2Fandrew%5Fbeatty%5Fatu%5Fie%2FDocuments%2Farchive%2Fpands%20private%20%282024%29%2Fslides%2FPANDS%201%2E2%20Running%20first%20python%2Emp4&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E07962988%2D253c%2D4376%2D95ae%2Df00070016457
 
-#----------End----------
+#----------End----------#
